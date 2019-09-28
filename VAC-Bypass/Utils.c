f97d88851b7010998b66f7465ab8605c3ba3411a
Daniel Krupiński <danielkrupinski@outlook.com>
2019-09-28 18:01:33 +0200

    Update Utils_hookImport() function

--- VAC-Bypass/Utils.c
+++ VAC-Bypass/Utils.c
@@ -33,26 +33,28 @@ VOID Utils_hookImport(PCWSTR moduleName, PCSTR importModuleName, PCSTR functionN
 {
     PBYTE module = (PBYTE)GetModuleHandleW(moduleName);
 
-    PIMAGE_NT_HEADERS ntHeaders = (PIMAGE_NT_HEADERS)(module + ((PIMAGE_DOS_HEADER)module)->e_lfanew);
-    PIMAGE_IMPORT_DESCRIPTOR imports = (PIMAGE_IMPORT_DESCRIPTOR)(module + ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
+    if (module) {
+        PIMAGE_NT_HEADERS ntHeaders = (PIMAGE_NT_HEADERS)(module + ((PIMAGE_DOS_HEADER)module)->e_lfanew);
+        PIMAGE_IMPORT_DESCRIPTOR imports = (PIMAGE_IMPORT_DESCRIPTOR)(module + ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
 
-    for (PIMAGE_IMPORT_DESCRIPTOR import = imports; import->Name; import++) {
-        if (_strcmpi(module + import->Name, importModuleName))
-            continue;
-
-        for (PIMAGE_THUNK_DATA original_first_thunk = (PIMAGE_THUNK_DATA)(module + import->OriginalFirstThunk), first_thunk = (PIMAGE_THUNK_DATA)(module + import->FirstThunk); original_first_thunk->u1.AddressOfData; original_first_thunk++, first_thunk++) {
-            if (strcmp((PCSTR)((PIMAGE_IMPORT_BY_NAME)(module + original_first_thunk->u1.AddressOfData))->Name, functionName))
+        for (PIMAGE_IMPORT_DESCRIPTOR import = imports; import->Name; import++) {
+            if (_strcmpi(module + import->Name, importModuleName))
                 continue;
 
-            PDWORD functionAddress = &first_thunk->u1.Function;
+            for (PIMAGE_THUNK_DATA original_first_thunk = (PIMAGE_THUNK_DATA)(module + import->OriginalFirstThunk), first_thunk = (PIMAGE_THUNK_DATA)(module + import->FirstThunk); original_first_thunk->u1.AddressOfData; original_first_thunk++, first_thunk++) {
+                if (strcmp((PCSTR)((PIMAGE_IMPORT_BY_NAME)(module + original_first_thunk->u1.AddressOfData))->Name, functionName))
+                    continue;
 
-            DWORD old;
-            if (VirtualProtect(functionAddress, sizeof(fun), PAGE_READWRITE, &old)) {
-                *functionAddress = (DWORD)fun;
-                VirtualProtect(functionAddress, sizeof(fun), old, &old);
+                PDWORD functionAddress = &first_thunk->u1.Function;
+
+                DWORD old;
+                if (VirtualProtect(functionAddress, sizeof(fun), PAGE_READWRITE, &old)) {
+                    *functionAddress = (DWORD)fun;
+                    VirtualProtect(functionAddress, sizeof(fun), old, &old);
+                }
+                break;
             }
             break;
         }
-        break;
     }
 }
